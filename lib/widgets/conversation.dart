924fd1112ab6f7033893c32a8c683f96b63b952d
Developer-felix <onjombafelix1@gmail.com>
2021-07-26 08:31:33 -0700

    Change online boolean style

--- lib/widgets/conversation.dart
+++ lib/widgets/conversation.dart
@@ -5,21 +5,25 @@ class Conversation extends StatelessWidget {
   final name;
   final msg;
   final isRead;
-  const Conversation(this.isRead,{ Key? key,this.avatar,this.name,this.msg }) : super(key: key);
+  final time;
+  const Conversation(this.isRead,{ Key? key,this.avatar,this.name,this.msg,this.time }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
                 leading:  CircleAvatar(radius: 25, backgroundImage: AssetImage("images/$avatar"),),
                 title: Text("$name"),
-                subtitle: Text("$msg"),
-                trailing:Container(height: 12,width: 12,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white,width: 1),
-                          borderRadius: BorderRadius.circular(20),
-                          color: isRead? Colors.white : Colors.blue
-                          ),
-                        ), 
+                subtitle: Text("$msg",style: isRead? null : TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),
+                // trailing:Container(height: 12,width: 12,
+                //         decoration: BoxDecoration(
+                //           border: Border.all(color: Colors.white,width: 1),
+                //           borderRadius: BorderRadius.circular(20),
+                //           color: isRead? Colors.white : Colors.blue
+                //           ),
+                //         ), 
+                trailing: Text("$time"),
+               
+                 
               );
   }
 }
